bb5ccd4a38a288ba57013c0537e3b6415defc928
Your NameCoreAngel <bragon912@gmail.com>
2018-05-12 23:17:01 +0200

    Add RWD

--- src/app/header/navigation/navigation.component.ts
+++ src/app/header/navigation/navigation.component.ts
@@ -12,4 +12,29 @@ export class NavigationComponent implements OnInit {
   ngOnInit() {
   }
 
+  clickMenu() {
+
+    let navigation = document.querySelector("#navigation");
+    let button = document.querySelector("#nav__button");
+
+    if(navigation.classList.contains("nav__first-list--open")){
+      button.setAttribute("aria-expanded", "false");
+      navigation.classList.remove("nav__first-list--open");
+
+      setTimeout(() => {
+        navigation.classList.remove("nav__first-list--visible");
+      }, 300);
+
+    } else{
+      button.setAttribute("aria-expanded", "true");
+      navigation.classList.add("nav__first-list--visible");
+
+      setTimeout(() => {
+        navigation.classList.add("nav__first-list--open");
+      }, 20);
+      
+    }
+
+  }
+
 }
